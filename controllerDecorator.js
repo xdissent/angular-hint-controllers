1ac03d81a5ec5ab572d376fc78bc0885d9bb779a
Erin Altenhof-Long <ealtenhof@google.com>
2014-07-07 10:35:19 -0700

    feat(controllerDecorator): add detection of controllers instantiated on the window

--- controllerDecorator.js
+++ controllerDecorator.js
@@ -2,6 +2,8 @@
 
 'use strict';
 
+var nameToControllerMatch = {};
+var controllers = {};
 /**
 * Decorates $controller with a patching function to
 * throw an error if DOM APIs are manipulated from
@@ -10,19 +12,35 @@
 angular.module('ngHintController', []).
   config(function ($provide) {
     $provide.decorator('$controller', function($delegate, $injector) {
-      return function(ctrl, locals) {
-        if(angular.isString(ctrl)) {
-          match = expression.match(CNTRL_REG),
-          constructor = match[1],
-          identifier = match[3];
-          ctrl = controllers.hasOwnProperty(constructor)
-            ? controllers[constructor]
-            : console.log('Not on window') || console.log('It is against Angular best practices to instantiate a controller on the window.');
+        return function(ctrl, locals) {
+          if(typeof ctrl == 'string') {
+            ctrl = nameToControllerMatch[ctrl];
+          }
+          // patch methods on $scope
+          if (!locals) {
+           locals = {};
+          }
+          //If the controller is not in the list of already registered controllers
+          //and it is not connected to the local scope, it must be instantiated on the window
+          if(controllers[ctrl] == undefined && (!locals.$scope || !locals.$scope[ctrl])) {
+              console.log('It is against Angular best practices to instantiate a controller on the window.');
+          }
+          var ctrlInstance = $delegate.apply(this, [ctrl, locals]);
+          return ctrlInstance;
         }
-        var ctrlInstance = $delegate.apply(this, [ctrl, locals]);
-        return ctrlInstance;
-      }
     });
-  });
+});
+
+var originalModule = angular.module;
+angular.module = function () {
+  var module = originalModule.apply(this, arguments);
+  var originalController = module.controller;
+  module.controller = function(controllerName, controllerConstructor) {
+    nameToControllerMatch[controllerName] = controllerConstructor;
+    controllers[controllerConstructor] = controllerConstructor;
+    return originalController.apply(this, arguments);
+  };
+  return module;
+}
 
 }());
--- controllerDecorator_test.js
+++ controllerDecorator_test.js
@@ -9,11 +9,18 @@ describe('controllerDecorator', function() {
 
   it('should detect if a controller is instantiated on the window', function() {
     spyOn(console, 'log');
-    var controllerMock = function() {
+    window.controllerMock = function() {
         var element = document.createElement('a');
         element.innerHTML = 'testValue';
     };
-    var sampleControl = $controller(controllerMock);
+    window.sampleControl = $controller(controllerMock);
     expect(console.log).toHaveBeenCalledWith('It is against Angular best practices to instantiate a controller on the window.');
+  });
+
+  it('should not log a message if the controller is on the local scope', function() {
+    spyOn(console, 'log');
+    angular.module('SampleApp', []).controller('SampleController', function() {});
+    var ctrl = $controller('SampleController');
+    expect(console.log).not.toHaveBeenCalled();
   });
 });
