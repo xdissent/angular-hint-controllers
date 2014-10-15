635aadb14f654fcff2319444cc6d5c974bd0b026
Brian Ford <btford@umich.edu>
2014-10-15 16:36:06 -0700

    refactor(controllerDecorator): further DRY tests

--- hint-controllers_test.js
+++ hint-controllers_test.js
@@ -12,7 +12,12 @@ describe('controllerDecorator', function() {
     $controller = _$controller_;
     $rootScope = _$rootScope_;
     $compile = _$compile_;
+    spyOn(hintLog, 'logMessage').andCallThrough();
   }));
+
+  afterEach(function () {
+    hintLog.flush();
+  })
 
   function MockController() {
     var element = document.createElement('a');
@@ -20,7 +25,6 @@ describe('controllerDecorator', function() {
   }
 
   it('should detect if a controller is instantiated on the window', function() {
-    spyOn(hintLog, 'logMessage');
     var sampleControl = $controller(MockController);
     if (angular.version.minor < 3) {
       expect(hintLog.logMessage).toHaveBeenCalledWith('Controllers', 'It is against Angular best ' +
@@ -34,62 +38,63 @@ describe('controllerDecorator', function() {
 
 
   it('should not warn about angular internal controllers instantiated on the window', function() {
-    spyOn(hintLog, 'logMessage');
     var scope = $rootScope.$new();
-      angular.module('SampleApp', []).controller('SampleController', function($scope) {
-        $scope.types = [
-            { name: 'Controllers', isChecked: false},
-            { name: 'Directives', isChecked: false},
-            { name: 'DOM', isChecked: false},
-            { name: 'Events', isChecked: false},
-            { name: 'Interpolation', isChecked: false},
-            { name: 'Modules', isChecked: false}
-          ];
-      });
-      var ctrl = $controller('SampleController', {$scope: scope});
-      var elm = angular.element('<div ng-controller="SampleController">' +
-                                  '<span ng-repeat="type in types">' +
-                                    '<input  type="checkbox" id="{{type.name}}" ng-click="changeList()" ng-model="type.isChecked">' +
-                                      '{{type.name}}' +
-                                  '</span>' +
-                                  '<form></form>' +
-                                '</div>');
-      $compile(elm)(scope);
-      $rootScope.$digest();
-      expect(hintLog.logMessage).not.toHaveBeenCalled();
+    angular.module('SampleApp', []).controller('SampleController', function($scope) {
+      $scope.types = [
+        { name: 'Controllers', isChecked: false},
+        { name: 'Directives', isChecked: false},
+        { name: 'DOM', isChecked: false},
+        { name: 'Events', isChecked: false},
+        { name: 'Interpolation', isChecked: false},
+        { name: 'Modules', isChecked: false}
+      ];
+    });
+    var ctrl = $controller('SampleController', {$scope: scope});
+    var elm = angular.element('<div ng-controller="SampleController">' +
+                                '<span ng-repeat="type in types">' +
+                                  '<input  type="checkbox" id="{{type.name}}" ng-click="changeList()" ng-model="type.isChecked">' +
+                                    '{{type.name}}' +
+                                '</span>' +
+                                '<form></form>' +
+                              '</div>');
+    $compile(elm)(scope);
+    $rootScope.$digest();
+    expect(hintLog.logMessage).not.toHaveBeenCalled();
   });
 
 
   it('should allow the instantiation of global controller functions', function() {
-    spyOn(hintLog, 'logMessage');
     var scope = $rootScope.$new();
-      window.GlobalFunction = function GlobalFunction($scope) {
-        $scope.types = [
-            { name: 'Controllers', isChecked: false},
-            { name: 'Directives', isChecked: false},
-            { name: 'DOM', isChecked: false},
-            { name: 'Events', isChecked: false},
-            { name: 'Interpolation', isChecked: false},
-            { name: 'Modules', isChecked: false}
-          ];
-      }
-      var ctrl = $controller('GlobalFunction', {$scope: scope});
-      var elm = angular.element('<div ng-controller="GlobalFunction">' +
-                                  '<span ng-repeat="type in types">' +
-                                    '<input  type="checkbox" id="{{type.name}}" ng-click="changeList()" ng-model="type.isChecked">' +
-                                      '{{type.name}}' +
-                                  '</span>' +
-                                  '<form></form>' +
-                                '</div>');
-      expect(function() {
-        $compile(elm)(scope);
-        $rootScope.$digest();
-      }).not.toThrow();
+
+    window.GlobalFunction = function GlobalFunction($scope) {
+      $scope.types = [
+        { name: 'Controllers', isChecked: false},
+        { name: 'Directives', isChecked: false},
+        { name: 'DOM', isChecked: false},
+        { name: 'Events', isChecked: false},
+        { name: 'Interpolation', isChecked: false},
+        { name: 'Modules', isChecked: false}
+      ];
+    };
+
+    var ctrl = $controller('GlobalFunction', {$scope: scope});
+    var elm = angular.element('<div ng-controller="GlobalFunction">' +
+                                '<span ng-repeat="type in types">' +
+                                  '<input  type="checkbox" id="{{type.name}}" ng-click="changeList()" ng-model="type.isChecked">' +
+                                    '{{type.name}}' +
+                                '</span>' +
+                                '<form></form>' +
+                              '</div>');
+    expect(function() {
+      $compile(elm)(scope);
+      $rootScope.$digest();
+    }).not.toThrow();
+
+    delete window.GlobalFunction;
   });
 
 
   it('should still warn about the naming of global controller functions', function() {
-    spyOn(hintLog, 'logMessage');
     var scope = $rootScope.$new();
     window.globalFunction = function globalFunction($scope) {
       $scope.types = [
@@ -115,25 +120,29 @@ describe('controllerDecorator', function() {
       'controllers with an uppercase first letter. Check the name of \'globalFunction\'.', SEVERITY_WARNING);
     expect(hintLog.logMessage).toHaveBeenCalledWith('Controllers', 'The best practice is to name ' +
       'controllers ending with \'Controller\'. Check the name of \'globalFunction\'.', SEVERITY_WARNING);
+
+    delete window.globalFunction;
   });
 
 
   it('should throw a helpful error if the controller cannot be created', function($scope) {
-      var scope = $rootScope.$new();
-      window.GlobalFunction = function GlobalFunction($scope) {
-        $scope.types = [
-            { name: 'Controllers', isChecked: false},
-            { name: 'Directives', isChecked: false},
-            { name: 'DOM', isChecked: false},
-            { name: 'Events', isChecked: false},
-            { name: 'Interpolation', isChecked: false},
-            { name: 'Modules', isChecked: false}
-          ];
-      }
-      expect(function() {
-        var ctrl = $controller('NotTheGlobalFunction', {$scope: scope});
-      }).toThrow('The controller function for NotTheGlobalFunction could not be found. ' +
-          'Is the function registered under that name?');
+    var scope = $rootScope.$new();
+    window.GlobalFunction = function GlobalFunction($scope) {
+      $scope.types = [
+        { name: 'Controllers', isChecked: false},
+        { name: 'Directives', isChecked: false},
+        { name: 'DOM', isChecked: false},
+        { name: 'Events', isChecked: false},
+        { name: 'Interpolation', isChecked: false},
+        { name: 'Modules', isChecked: false}
+      ];
+    }
+    expect(function() {
+      var ctrl = $controller('NotTheGlobalFunction', {$scope: scope});
+    }).toThrow('The controller function for NotTheGlobalFunction could not be found. ' +
+        'Is the function registered under that name?');
+
+    delete window.GlobalFunction;
   });
 
 
@@ -141,17 +150,16 @@ describe('controllerDecorator', function() {
     if (angular.version.minor < 3) {
       return;
     }
-    spyOn(hintLog, 'logMessage');
-    var sampleControl = $controller(MockController);
+
+    $controller(MockController);
     expect(hintLog.logMessage).toHaveBeenCalledWith('Controllers', 'Global instantiation of ' +
       'controllers was deprecated in Angular 1.3.0. Define the controller on a module.', SEVERITY_ERROR);
   });
 
 
   it('should not log a message if the controller is on the local scope', function() {
-    spyOn(hintLog, 'logMessage');
     angular.module('SampleApp', []).controller('SampleController', function() {});
-    var ctrl = $controller('SampleController');
+    $controller('SampleController');
     expect(hintLog.logMessage).not.toHaveBeenCalledWith('Controllers', 'It is against Angular' +
       'best practices to instantiate a controller on the window. This behavior is deprecated in' +
       ' Angular 1.3.0', (angular.version.minor < 3 ? SEVERITY_WARNING : SEVERITY_ERROR));
@@ -159,44 +167,39 @@ describe('controllerDecorator', function() {
 
 
   it('should warn if a controller name does not begin with an uppercase letter', function(){
-    spyOn(hintLog, 'logMessage');
     angular.module('SampleApp', []).controller('sampleController', function() {});
-    var ctrl = $controller('sampleController');
+    $controller('sampleController');
     expect(hintLog.logMessage).toHaveBeenCalledWith('Controllers', 'The best practice is to name ' +
       'controllers with an uppercase first letter. Check the name of \'sampleController\'.', SEVERITY_WARNING);
   });
 
 
   it('should not warn if a controller name begins with an uppercase letter', function(){
-    spyOn(hintLog, 'logMessage');
     angular.module('SampleApp', []).controller('SampleController', function() {});
-    var ctrl = $controller('SampleController');
+    $controller('SampleController');
     expect(hintLog.logMessage).not.toHaveBeenCalled();
   });
 
 
   it('should warn if a controller name does not include Controller', function(){
-    spyOn(hintLog, 'logMessage');
     angular.module('SampleApp', []).controller('Sample', function() {});
-    var ctrl = $controller('Sample');
+    $controller('Sample');
     expect(hintLog.logMessage).toHaveBeenCalledWith('Controllers', 'The best practice is to name ' +
       'controllers ending with \'Controller\'. Check the name of \'Sample\'.', SEVERITY_WARNING);
   });
 
 
   it('should warn if a controller name does not end with Controller', function(){
-    spyOn(hintLog, 'logMessage');
     angular.module('SampleApp', []).controller('SampleControllerYay', function() {});
-    var ctrl = $controller('SampleControllerYay');
+    $controller('SampleControllerYay');
     expect(hintLog.logMessage).toHaveBeenCalledWith('Controllers', 'The best practice is to name ' +
       'controllers ending with \'Controller\'. Check the name of \'SampleControllerYay\'.', SEVERITY_WARNING);
   });
 
 
   it('should not warn if a controller ends with Controller', function(){
-    spyOn(hintLog, 'logMessage');
     angular.module('SampleApp', []).controller('SampleController', function() {});
-    var ctrl = $controller('SampleController');
+    $controller('SampleController');
     expect(hintLog.logMessage).not.toHaveBeenCalled();
   });
 
@@ -204,8 +207,8 @@ describe('controllerDecorator', function() {
   it('should collect all hinting messages using hintLog', function() {
     var sampleControl = $controller(MockController);
     angular.module('SampleApp', []).controller('sample', function() {});
-    var ctrl = $controller('sample');
+    $controller('sample');
     var log = hintLog.flush();
-    expect(log['Controllers']['Warning Messages'].length).toBe(4);
+    expect(log['Controllers']['Warning Messages'].length).toBe(2);
   });
 });
